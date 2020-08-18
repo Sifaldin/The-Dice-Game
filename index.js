db2fa8efb6c7928147571a7d50b6578bb1316624
Sifaldin Abbas <57807968+Sifaldin@users.noreply.github.com>
2020-08-18 10:57:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNum1 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img1").setAttribute("src", "images/dice"+randomNum1+".png");
+document.querySelector(".img1").setAttribute("src", "dice"+randomNum1+".png");
 var randomNum2 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img2").setAttribute("src", "images/dice"+randomNum2+".png");
+document.querySelector(".img2").setAttribute("src", "dice"+randomNum2+".png");
 
 if (randomNum1 > randomNum2){
   document.querySelector("h1").innerHTML = "Player 1 won 🤣!";
